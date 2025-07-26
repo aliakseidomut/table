ccfc75d84361fedbf4e959026677bceeedfa3b59
aliakseidomut <alexeydomut@gmail.com>
2025-07-26 13:40:47 +0300

    feat: add table

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,49 @@
+import { Table, Space, Button } from "antd";
+import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
+
+const dataSource = [];
+
+const columns = [
+  {
+    title: "Имя",
+    dataIndex: "name",
+    key: "name",
+  },
+  {
+    title: "Дата",
+    dataIndex: "date",
+    key: "date",
+  },
+  {
+    title: "Число",
+    dataIndex: "number",
+    key: "number",
+  },
+  {
+    title: "Действия",
+    key: "actions",
+    render: () => (
+      <Space size="middle">
+        <Button color="cyan" variant="solid">
+          <EditOutlined />
+        </Button>
+        <Button color="danger" variant="solid">
+          <DeleteOutlined />
+        </Button>
+      </Space>
+    ),
+  },
+];
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Table
+      columns={columns}
+      dataSource={dataSource}
+      pagination={false}
+      bordered={true}
+    />
+  );
 }
 
 export default App;
